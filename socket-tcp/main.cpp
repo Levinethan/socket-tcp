1718b04c565dcbac7d0e7b82fba1308d8d3539a1
Man <48085482+Levinethan@users.noreply.github.com>
2021-03-02 11:12:37 +0800

    循环server
    服务端死循环接受client

--- socket-tcp/main.cpp
+++ socket-tcp/main.cpp
@@ -16,33 +16,49 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
+#define INVALID_SOCKET  0
+int _sock;
+int _cSock;
 
 int main(int argc, const char * argv[]) {
-    int _sock;
-    char recvBuf[256] = {};
-    if ((_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
-            fprintf(stderr,"Socket error:%s\n\a", strerror(errno));
-            exit(1);
-        }
+    //建立一个socket IPV4 创建面向数据流 tcp/udp
+    _sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     sockaddr_in _sin = {};
     //绑定client连接的网络接口
     _sin.sin_family = AF_INET;
     _sin.sin_port = htons(4500);
     _sin.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    
-    int ret = connect(_sock, (sockaddr *)&_sin, sizeof(sockaddr));
-    if (ret == -1) {
-        printf("error\n");
+    if (bind(_sock, (sockaddr *)&_sin, sizeof(_sin)) < 0){
+        perror("bind error");
+        exit(1);
     }else{
-        printf("success connect server\n");
+        printf("success bind\n");
+    }
+    //监听端口
+    if (listen(_sock,5) < 0){
+        perror("listen error\n");
+        exit(1);
+    }else{
+        printf("success listen\n");
     }
     
-    long nlen = recv(_sock, recvBuf, 256, 0);
-    if (nlen > 0){
-        printf("receive data :%s \n",recvBuf);
+    //accept 等待client连接
+    
+    _cSock = INVALID_SOCKET;
+    while (1) {
+        _cSock = accept(_sock, (sockaddr *)NULL ,NULL);
+        
+        
+        if (INVALID_SOCKET == _sock){
+            printf("invalid client socket\n");
+        }
+        printf("success accept\n");
+        //发送消息
+        char msgBuf[] = "I`m Server";
+        send(_cSock, msgBuf, strlen(msgBuf)+1, 0);
     }
+    
+    //关闭socket
     close(_sock);
-    getchar();
     return 0;
 }
