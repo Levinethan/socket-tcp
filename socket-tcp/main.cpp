fe0b80d9a2de7cfb348f3be6be3fc1a1528b72db
Man <48085482+Levinethan@users.noreply.github.com>
2021-03-02 11:02:30 +0800

    server
    tcp服务端

--- socket-tcp/main.cpp
+++ socket-tcp/main.cpp
@@ -6,9 +6,43 @@
 //
 
 #include <iostream>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <signal.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int _sock;
+    char recvBuf[256] = {};
+    if ((_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
+            fprintf(stderr,"Socket error:%s\n\a", strerror(errno));
+            exit(1);
+        }
+    sockaddr_in _sin = {};
+    //绑定client连接的网络接口
+    _sin.sin_family = AF_INET;
+    _sin.sin_port = htons(4500);
+    _sin.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    
+    int ret = connect(_sock, (sockaddr *)&_sin, sizeof(sockaddr));
+    if (ret == -1) {
+        printf("error\n");
+    }else{
+        printf("success connect server\n");
+    }
+    
+    long nlen = recv(_sock, recvBuf, 256, 0);
+    if (nlen > 0){
+        printf("receive data :%s \n",recvBuf);
+    }
+    close(_sock);
+    getchar();
     return 0;
 }
